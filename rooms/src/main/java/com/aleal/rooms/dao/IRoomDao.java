bb82f12935d74991b421254322c1c2763ba09fb2
diego <diegocarrp@gmail.com>
2023-06-22 22:44:17 -0400

    fisrt adjustments

--- rooms/src/main/java/com/aleal/rooms/dao/IRoomDao.java
+++
@@ -1,9 +0,0 @@
-package com.aleal.rooms.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.aleal.rooms.model.Room;
-
-public interface IRoomDao extends CrudRepository<Room, Long>{
-
-}
